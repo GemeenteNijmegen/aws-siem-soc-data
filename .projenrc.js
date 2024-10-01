178144ac6d41acec7ccbfdf9a7a0cbe6945589d8
Wieteke <149399985+WietekeNijmegen@users.noreply.github.com>
2024-10-01 12:56:58 +0200

    feat: upgrade projen and readme

--- .projenrc.js
+++ .projenrc.js
@@ -2,14 +2,16 @@ const { GemeenteNijmegenCdkApp } = require('@gemeentenijmegen/projen-project-typ
 const project = new GemeenteNijmegenCdkApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  devDeps: ['@gemeentenijmegen/projen-project-type'],
   name: 'aws-siem-soc-data',
+  repository: 'https://github.com/GemeenteNijmegen/aws-siem-soc-data.git',
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'], // No acceptance branch
+    },
+  },
+  devDeps: ['@gemeentenijmegen/projen-project-type'],
   deps: [
     '@gemeentenijmegen/aws-constructs',
   ],
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # aws-siem-soc-data
+
+S3 bucket ter ondersteuning van applicatie genaamd chronical van iRvN. Staat in landingzone onder irvn account. 
+Contact: iRvN beveiliging.
+
